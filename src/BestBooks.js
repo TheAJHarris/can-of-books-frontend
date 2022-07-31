2e9212f82463dfc94e10c5f2d7d2965f0b73791f
Hayden Cleaver <rexcleaver90@gmail.com>
2022-07-31 19:06:50 -0400

    carousel completed

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,4 +1,8 @@
 import React from 'react';
+import {Carousel} from 'react-bootstrap';
+import axios from 'axios';
+
+let herokuURL = 'https://can-of-books-ajh-hc.herokuapp.com/books';
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -8,24 +12,48 @@ class BestBooks extends React.Component {
     }
   }
 
-  /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  getBooks = () => {
+      axios.get(herokuURL).then(response => {
+        this.setState({
+                books: response.data
+              })
+      })
+      .catch(e => {
+        let message = `${e.response.data.error}. ${e.message} ${e.code}.`;
+        console.log(message);
+      })
+      
+  }
 
+  componentDidMount = () => {
+    this.getBooks();
+  }
   render() {
 
-    /* TODO: render all the books in a Carousel */
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
+        
         {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
+        <Carousel>
+          {this.state.books.map(element => 
+            <Carousel.Item key={element._id}>
+              <img src="https://place-hold.it/2000x400/White/Black.jpeg" alt={element.title}/>
+              <Carousel.Caption style={{margin: "20px"}}><h4>{element.title}</h4></Carousel.Caption>
+              <Carousel.Caption>{element.description}</Carousel.Caption>
+
+            </Carousel.Item>
+          )}
+        </Carousel>
         ) : (
           <h3>No Books Found :(</h3>
         )}
+        
       </>
     )
   }
 }
 
 export default BestBooks;
+
